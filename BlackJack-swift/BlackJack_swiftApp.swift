ae5a6ba7a5b9ab26681ee3f1a93a54e1ea06f476
Theo Sementa <theosementa@gmail.com>
2025-06-21 20:50:35 +0200

    Initial Commit

---
+++ BlackJack-swift/BlackJack_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackJack_swiftApp.swift
+//  BlackJack-swift
+//
+//  Created by Theo Sementa on 21/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct BlackJack_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlackJack-swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BlackJack-swift
+//
+//  Created by Theo Sementa on 21/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
